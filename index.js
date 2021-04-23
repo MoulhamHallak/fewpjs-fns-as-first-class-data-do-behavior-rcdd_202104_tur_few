4d9886524ab2f49e11a6bceda350ba01321b498f
Moulham Hallak <mou990@gmail.com>
2021-04-23 20:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+// const time = parseInt(timeString);
+const sHours = timeString.split(':')[0];
+const sMinutes = timeString.split(':')[1];
+let greetSen;
 /* Write your implementation of greet() */
+function greet(sHours){
+  if(sHours < "12"){
+    return "Good Morning";
+  }else if (sHours > "12" && sHours < "17"){
+    return "Good Afternoon";
+  } else if (sHours > "17"){
+    return "Good Evening";
+  }
+  return greetSen;
+}
+
 /* Write your implementation of displayMessage() */
+let content = "greetSen"
+function displayMessage(testContent){
+  let greetcontent = document.getElementById("greeting").innerText
+      expect(greetcontent).to.equal(content);
+}
